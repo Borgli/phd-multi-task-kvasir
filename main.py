e8c9213240e071c80fdde4e50983a438e337f012
Hanna Borgli <borgli.hanna@gmail.com>
2023-02-19 19:32:45 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,124 @@
+import pathlib
+
+import keras
+import skimage.io
+from keras.models import Model
+from keras.layers import Input, Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+
+from keras.utils import plot_model, to_categorical
+import numpy as np
+import os
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
+import tensorflow as tf
+from skimage.io import imread_collection
+
+from model import create_model
+
+BATCH_SIZE = 32
+IMG_HEIGHT = 256
+IMG_WIDTH = 256
+
+DATASET_DIR = pathlib.Path("hyper-kvasir/segmented-images")
+MASK_DATA = DATASET_DIR.joinpath("masks")
+CLASS_DATA = DATASET_DIR.joinpath("images")
+'''
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    CLASS_DATA,
+    labels=None,
+    color_mode="grayscale",
+    #validation_split=0.2,
+    #subset="both",
+    seed=123,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE
+)
+
+mask_ds = tf.keras.utils.image_dataset_from_directory(
+    MASK_DATA,
+    labels=None,
+    color_mode="grayscale",
+    seed=123,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE
+)
+'''
+model = create_model()
+
+'''
+train_ds = train_ds.unbatch()
+mask_ds = mask_ds.unbatch()
+
+labels = list(map(lambda x: 1, train_ds))
+
+train_ds = list(train_ds.as_numpy_iterator())
+mask_ds = list(mask_ds.as_numpy_iterator())
+'''
+
+training_data = []
+labels = []
+CLASS_DATA = CLASS_DATA.joinpath("polyps")
+for file in os.listdir(CLASS_DATA):
+    training_data.append(skimage.transform.resize(skimage.io.imread(CLASS_DATA.joinpath(file), as_gray=True), (256, 256, 1)))
+    labels.append(1)
+
+mask = []
+for file in os.listdir(MASK_DATA):
+    mask.append(skimage.transform.resize(skimage.io.imread(MASK_DATA.joinpath(file), as_gray=True), (256, 256, 1)))
+
+
+train = training_data[(len(training_data)//100) * 20:]
+test = training_data[:(len(training_data)//100) * 20]
+
+train_labels = labels[(len(labels)//100) * 20:]
+test_labels = labels[:(len(labels)//100) * 20]
+
+train_mask = mask[(len(mask)//100) * 20:]
+test_mask = mask[:(len(labels)//100) * 20]
+
+#train = imread_collection(os.listdir(CLASS_DATA.joinpath("polyps")))
+
+'''
+try:
+    history = model.fit({'input': np.array(train)},
+                        {'classification': np.array(labels), 'segmentation': np.array(mask)},
+                        epochs=50, batch_size=BATCH_SIZE,
+                        verbose=1
+                        )
+
+except KeyboardInterrupt:
+    pass
+'''
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                              save_weights_only=True,
+                                              verbose=1)
+
+history = model.fit({'input': np.array(train)},
+                    {'classification': np.array(train_labels), 'segmentation': np.array(train_mask)},
+                    epochs=50, batch_size=BATCH_SIZE,
+                    verbose=1,
+                    validation_data=({'input': np.array(test)},
+                    {'classification': np.array(test_labels), 'segmentation': np.array(test_mask)}),
+                    callbacks=[cp_callback]
+                    )
+
+# Train the model with the new callback
+#model.fit(train_images,
+#          train_labels,
+#          epochs=10,
+#          validation_data=(test_images, test_labels),
+#          callbacks=[cp_callback])  # Pass callback to training
+
